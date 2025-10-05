53a66983a0b9303af4ee9598c8dea07eabc4a8ec
himanshusssingh <himanshusingh39397@gmail.com>
2025-10-05 16:07:42 +0530

    Creat an express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config({ path: './.env' });
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port: https://localhost:${port}`);
+})
