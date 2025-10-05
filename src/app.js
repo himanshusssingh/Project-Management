ad45577fe4219e160d9bf0316458d78cb7ece069
himanshusssingh <himanshusingh39397@gmail.com>
2025-10-05 22:55:37 +0530

    Add CORS configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './.env' });
 
 
 const app = express();
@@ -7,6 +11,14 @@ const app = express();
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"))
+
+//CORS Configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",")  || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorisaton"],
+}))
 
 
 
