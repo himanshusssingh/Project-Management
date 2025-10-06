df54233b52a7f6a4ee9937d3e7cfd2336fb5a4ef
himanshusssingh <himanshusingh39397@gmail.com>
2025-10-06 16:48:06 +0530

    Add healthchech route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import  healthcheckRouter  from './routes/healthcheck.routes.js';
 
 
 const app = express();
@@ -16,6 +17,9 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorisaton"],
 }))
+
+//healthcheck router
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 
 app.get("/", (req, res) => {
---
+++ src/controllers/healthcheck.controllers.js
@@ -0,0 +1,11 @@
+import { ApiResponse } from '../utils/api-responce.js';
+import { asyncHandler } from '../utils/async-handler.js';
+
+const healthCheck = asyncHandler((req, res) => {
+    res.status(200).json(
+        new ApiResponse(200, {message: "Server is running."})
+    );
+});
+
+
+export {healthCheck};
