d1b6adb2d42a8e5319dc8ef5287aed95f7681a4b
himanshusssingh <himanshusingh39397@gmail.com>
2025-10-09 10:31:49 +0530

    Add feature for resend Email Verification

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -190,6 +190,38 @@ const verifyEmail = asyncHandler(async(req, res) => {
 });
 
 
+const resendEmailVerification = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if(!user) {
+        throw new ApiError(404, "User not found!");
+    }
+
+    if(!user.isEmailVerified) {
+        throw new ApiError(409, "Email already confirmed");
+    }
+
+    const {unHashedToken, hashedToken, tokenExpiry} = user.generateTemporaryToken();
+
+    user.emailVerificationToken = hashedToken;
+    user.emailVerificationExpiry = tokenExpiry;
+
+    await user.save({validateBeforeSave: false});
+
+    await sendEmail({
+        emai: user?.email,
+        subject: "Please verify your email.",
+        mailgenContent: emailVerificationMailgenContent(
+            user.username,
+            `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`,
+        ),
+    });
+
+    return res
+       .status(200)
+       .json(new ApiResponse(200, {}, "Mail has been sent to your email ID"));
+});
+
 
 export {
     registerUser,
