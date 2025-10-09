65f477a5ae88ad270d2cfa9d29864b276c39c767
himanshusssingh <himanshusingh39397@gmail.com>
2025-10-09 22:25:32 +0530

    Add all ligin related controllers

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -220,10 +220,153 @@ const resendEmailVerification = asyncHandler(async(req, res) => {
     return res
        .status(200)
        .json(new ApiResponse(200, {}, "Mail has been sent to your email ID"));
+}); 
+
+
+const refreshAccessToken = asyncHandler(async(req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if(!incomingRefreshToken) {
+        throw new ApiError(401, "Unathorised access.");
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+        );
+
+        const user = await User.findById(decodeToken?._id);
+        if(!user) {
+            throw new ApiError(401, "Invalid refresh token.");
+        }
+
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired.");
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true,
+        };
+
+        const {accessToken, refreshToken: newRefreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+        user.refreshToken = newRefreshToken;
+        await user.save();
+
+        return res
+          .status(200)
+          .cookie("accessToken", accessToken, options)
+          .cookie("refreshToken", newRefreshToken, options)
+          .json(
+            new ApiResponse(
+                200,
+                {accessToken, refreshToken: newRefreshToken},
+                "Access token refreshed",
+            ),
+          );
+    }
+    catch (err) {
+        throw new ApiError(401, "Invalid refresh token.");
+    }
+});
+
+const forgotPasswordRequest = asyncHandler(async (req, res) => {
+    const {email} = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user) {
+        throw new ApiError(404, "User does not exist.", []);
+    }
+
+    const {unHashedToken, hashedToken, tokenExpiry} = user.generateTemporaryToken();
+
+    user.forgotPasswordToken = hashedToken;
+    user.forgotPasswordExpiry = tokenExpiry;
+
+    await user.save({validateBeforeSave: false});
+
+    await sendEmail({
+      email: user?.email,
+      subject: "Password reset request",
+      mailgenContent: forgotPasswordMailgenContent(
+        user.username,
+        `${process.env.FORGOT_PASSWORD_REDIRECT_URL}/${unHashedToken}`,
+      ),
+    });
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+            200,
+            {},
+            "Password reset mail has been sent on your mail id.",
+        )
+      )
 });
 
 
+const resetForgotPassword = asyncHandler(async (req, res) => {
+  const { resetToken } = req.params;
+  const { newPassword } = req.body;
+
+  let hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    forgotPasswordToken: hashedToken,
+    forgotPasswordExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new ApiError(489, "Token is invalid or expired");
+  }
+
+  user.forgotPasswordExpiry = undefined;
+  user.forgotPasswordToken = undefined;
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password reset successfully"));
+});
+
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?._id);
+
+  const isPasswordValid = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordValid) {
+    throw new ApiError(400, "Invalid old Password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
 export {
-    registerUser,
-    login
-}
+  registerUser,
+  login,
+  logout,
+  getCurrentUser,
+  verifyEmail,
+  resendEmailVerification,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  changeCurrentPassword,
+  resetForgotPassword,
+};
